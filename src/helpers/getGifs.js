99e580830186b9d3152269c1f05cdb4067f29abe
Jair Jimenez Munevar <jairmunevar@gmail.com>
2021-04-04 12:57:48 -0500

    Docs added

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=x9N0Z7qQOWeXUBoql1bAbKZeDpVtPyPs`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=`;
     const response = await fetch(url);
     const { data } = await response.json();
 
